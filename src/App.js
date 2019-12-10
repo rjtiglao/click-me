ace5a3925c95b8ece89676a24cace7ee9a91e557
Rj Tiglao <rjtiglao@gmail.com>
2019-12-09 20:27:18 -0800

    change card game

--- src/App.js
+++ src/App.js
@@ -1,57 +1,66 @@
-import React, {Component} from 'react';
-import NavBar from './components/NavBar';
-import FootBar from './components/FootBar';
-import image from "./image.json";
-import ClickPic from "./components/ClickPic";
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import cards from "./cards.json";
 import "./App.css";
 
 class App extends Component {
+  // Setting this.state.cards to the cards json array
   state = {
+    cards,
     score: 0,
-    topscore: 0
-  };
-    // handleIncrement increments this.state.count by 1
-  handleIncrement = () => {
-    // We always use the setState method to update a component's state
-    this.setState({ score: this.state.score + 1 });
+    highscore: 0
   };
 
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({ highscore: this.state.score }, function() {
+        console.log(this.state.highscore);
+      });
+    }
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({ score: 0 });
+    return true;
+  };
+
+  clickCount = id => {
+    this.state.cards.find((o, i) => {
+      if (o.id === id) {
+        if (cards[i].count === 0) {
+          cards[i].count = cards[i].count + 1;
+          this.setState({ score: this.state.score + 1 }, function() {
+            console.log(this.state.score);
+          });
+          this.state.cards.sort(() => Math.random() - 0.5);
+          return true;
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  };
+  // Map over this.state.cards and render a cardCard component for each card object
   render() {
-    return(
-    
-    <div className="container">
-      <NavBar 
-        score={this.state.score}
-        topscore={this.state.topscore}
-      />
-      <div className="jumbotron">
-        <h1>Welcome to the Clicky Game!</h1>
-        <h3>Click on an image to earn points!</h3>
-      </div>
-
-        <div className="container">
-          <ClickPic 
-            image={image[0].image}
-            onClick={this.handleIncrement}
+    return (
+      <Wrapper>
+        <Header score={this.state.score} highscore={this.state.highscore}>
+          Clicky Game
+        </Header>
+        {this.state.cards.map(card => (
+          <Card
+            clickCount={this.clickCount}
+            id={card.id}
+            key={card.id}
+            image={card.image}
           />
-          <ClickPic 
-            image={image[1].image}
-            onClick={this.handleIncrement}
-          />
-          <ClickPic 
-            image={image[2].image}
-            onClick={this.handleIncrement}
-          />
-          <ClickPic 
-            image={image[3].image}
-            onClick={this.handleIncrement}
-          />
-      </div>
-
-      <FootBar />
-    </div>
-   )
+        ))}
+      </Wrapper>
+    );
   }
-};
+}
 
 export default App;
